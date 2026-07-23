0455b9c25ff15162950f1c84a0c2473c040636da
Fa-Wi <fabian-3-5@web.de>
2026-07-23 16:57:36 +0200

    smooth camera follow

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField]
-    private Vector3 offset;
+    private Vector3 cameraOffset;
     private PlayerMovement player;
+
+    private float sideScrollDistance = 2f;
+    [SerializeField]
+    private float smoothTime = 1;
+    [SerializeField]
+    private Vector3 velocity = new Vector3(0, 0, 5);
+    private Vector3 targetPos;
+
 
     void Start()
     {
@@ -13,6 +21,26 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, offset.z);
+        if (player.transform.position.x - transform.position.x < -sideScrollDistance)
+        {
+            targetPos = new Vector3(player.transform.position.x + sideScrollDistance, player.transform.position.y, cameraOffset.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+        }
+        else if (player.transform.position.x - transform.position.x > sideScrollDistance)
+        {
+            targetPos = new Vector3(player.transform.position.x - sideScrollDistance, player.transform.position.y, cameraOffset.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+        }
+        else if (player.transform.position.y - transform.position.y < -sideScrollDistance)
+        {
+            targetPos = new Vector3(cameraOffset.x, player.transform.position.y + sideScrollDistance, cameraOffset.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+        }
+        else if (player.transform.position.y - transform.position.y > sideScrollDistance)
+        {
+            targetPos = new Vector3(cameraOffset.x, player.transform.position.y - sideScrollDistance, cameraOffset.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+        }
+
     }
 }
