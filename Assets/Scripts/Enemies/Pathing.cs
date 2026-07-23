b85706b6c8036dbb92c26c31943cb463902f39c2
FabyWin <faby1winter@gmail.com>
2026-07-23 23:25:54 +0200

    default pathing for enemies on platforms

---
+++ Assets/Scripts/Enemies/Pathing.cs
@@ -0,0 +1,59 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Pathing : MonoBehaviour
+{
+    [SerializeField]
+    public Vector2 boxSize; //with a 1x1 sized object  x=1 and y=1 
+    [SerializeField]
+    private float castDist; // with a 1x1 sized object castDist = 1 
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+
+    void Update()
+    {
+        if (IsGroundAhead(transform.right) && transform.localScale.x > 0) //check right
+        {
+            //move right
+            transform.Translate(moveSpeed * Time.deltaTime * Vector2.right);
+        }
+        else if (transform.localScale.x > 0)
+        {
+            FlipScale();
+        }
+        if (IsGroundAhead(-transform.right) && transform.localScale.x < 0) // check left
+        {
+            //move left
+            transform.Translate(moveSpeed * Time.deltaTime * -Vector2.right);
+        }
+        else if (transform.localScale.x < 0)
+        {
+            FlipScale();
+        }
+
+
+    }
+
+    public bool IsGroundAhead(Vector3 direction)
+    {
+        if (Physics2D.BoxCast(transform.position + direction, boxSize, 0, -transform.up, castDist, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void FlipScale()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+}
