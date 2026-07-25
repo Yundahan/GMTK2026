08f3b5eaabedf85ba0983e325fda3f0a4fbcd042
FabyWin <faby1winter@gmail.com>
2026-07-26 01:07:38 +0200

    add volume slider to UI

--- Assets/Scripts/UI/ButtonScript.cs
+++ Assets/Scripts/UI/ButtonScript.cs
@@ -4,19 +4,16 @@ using UnityEngine.UI;
 public class ButtonScript : MonoBehaviour
 {
     [SerializeField]
-    private Button nextScene;
-    [SerializeField]
     private Button restart;
     void Awake()
     {
-        nextScene.onClick.AddListener(NextScene);
         restart.onClick.AddListener(Restart);
     }
 
-    private void NextScene()
-    {
-        SceneLoader.Instance().LoadScene(1 - SceneLoader.Instance().GetActiveSceneBuildIndex());
-    }
+    // private void NextScene()
+    // {
+    //     SceneLoader.Instance().LoadScene(1 - SceneLoader.Instance().GetActiveSceneBuildIndex());
+    // }
 
     private void Restart()
     {
