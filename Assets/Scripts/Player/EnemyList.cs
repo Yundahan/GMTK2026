125b228b068d832b920dc83a35b50309e3c930cc
Andrik Kleine <andrik.k@web.de>
2026-07-26 16:53:42 +0200

    animations

--- Assets/Scripts/Player/EnemyList.cs
+++ Assets/Scripts/Player/EnemyList.cs
@@ -10,6 +10,10 @@ public class EnemyList : MonoBehaviour
     private float levelEndDelay = 0.6f;
     [SerializeField]
     private string nextLevel = "Scene1";
+    [SerializeField]
+    private GameObject levelEndAnim;
+    [SerializeField]
+    private GameObject levelStartAnim;
 
     private List<EnemyNumber> allEnemies = new();
 
@@ -20,6 +24,7 @@ public class EnemyList : MonoBehaviour
 
     void Awake()
     {
+        Instantiate(levelStartAnim, transform.position, Quaternion.identity);
         playerSFX = GetComponent<PlayerSFX>();
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
@@ -41,6 +46,7 @@ public class EnemyList : MonoBehaviour
 
         if (allEnemies.Count == 0)
         {
+            Instantiate(levelEndAnim, Camera.main.transform.position, Quaternion.identity);
             allEnemiesDead = true;
             allEnemiesDeadTime = Time.time;
             Simulation.Instance().ToggleSimulation();
